f0f3ab4898282529aa82983f3869badf2de4fe92
hnidesu <q3375188791@hotmail.com>
2024-07-15 19:13:27 +0800

    refactor: - 优化了查询url参数的代码

--- TextUtil.cs
+++ TextUtil.cs
@@ -4,13 +4,13 @@
     {
         public static string? GetQueryValue(string url,string name)
         {
-            if (!url.Contains("?")) return null;
-            string query = url.Substring(url.LastIndexOf("?") + 1);
-            foreach(string item in query.Split("&"))
+            if (!url.Contains('?')) return null;
+            string query = url.Substring(url.LastIndexOf('?') + 1);
+            foreach(string item in query.Split('&'))
             {
                 string pair = item.Replace(" ", "");
-                if (pair.StartsWith(name + "="))
-                    return pair.Substring(pair.IndexOf('=') + 1);
+                if (pair.StartsWith(name + '='))
+                    return pair[(pair.IndexOf('=') + 1)..];
             }
             return null;
         }
