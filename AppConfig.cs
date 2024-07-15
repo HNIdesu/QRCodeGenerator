79bb11c714dc252bfb6d7442409a69ecedfcdf80
hnidesu <q3375188791@hotmail.com>
2024-07-15 19:17:10 +0800

    refactor(ConfigUtil): - 将ConfigUtil重命名为AppConfig并更新了接口调用 - 对部分字段进行了重命名 - 将部分字段设置为了只读

--- AppConfig.cs
+++ AppConfig.cs
@@ -4,45 +4,42 @@ using System.Net.Sockets;
 
 namespace QRCodeGenerator
 {
-    internal static class ConfigUtil
+    internal sealed class AppConfig
     {
-        private static ConfigManager _ConfigManager = ConfigManager.CreateOrOpenConfig("config");
-        private static string CurrentHostName = Dns.GetHostAddresses(Dns.GetHostName()).First((ip) => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
+        private static readonly ConfigManager mConfigManager = ConfigManager.CreateOrOpen("config");
+        private static readonly string mCurrentHostName = Dns.GetHostAddresses(Dns.GetHostName()).First((ip) => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
         public static string Host
         {
-            get => _ConfigManager.GetString("host", CurrentHostName);
+            get => mConfigManager.Get("host", mCurrentHostName);
             set
             {
-                var editor= _ConfigManager.Edit();
+                var editor= mConfigManager.Edit();
                 editor.Put("host", value);
                 editor.Commit();
-                _ConfigManager.Update();
             }
         }
 
         public static int Port
         {
-            get => _ConfigManager.GetInt32("port",2321);
+            get => mConfigManager.Get("port",2321);
             set
             {
-                var editor = _ConfigManager.Edit();
+                var editor = mConfigManager.Edit();
                 editor.Put("port", value);
                 editor.Commit();
-                _ConfigManager.Update();
             }
         }
 
         public static string? NetTraverseUrl
         {
-            get => _ConfigManager.GetString("net_traverse_url");
+            get => mConfigManager.Get("net_traverse_url",string.Empty);
             set
             {
                 if (value != null)
                 {
-                    var editor = _ConfigManager.Edit();
+                    var editor = mConfigManager.Edit();
                     editor.Put("net_traverse_url", value);
                     editor.Commit();
-                    _ConfigManager.Update();
                 }
                     
             }
@@ -51,13 +48,12 @@ namespace QRCodeGenerator
 
         public static bool EnableNetTraverse
         {
-            get => _ConfigManager.GetInt32("enable_net_traverse",0)==1;
+            get => mConfigManager.Get("enable_net_traverse",0)==1;
             set
             {
-                var editor = _ConfigManager.Edit();
+                var editor = mConfigManager.Edit();
                 editor.Put("enable_net_traverse", value?1:0);
                 editor.Commit();
-                _ConfigManager.Update();
             }
         }
 
